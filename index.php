edb7ccf213123ef8e4f9ad2104745fa49d3db319
ajz27 <113381790+ajz27@users.noreply.github.com>
2025-02-15 22:27:03 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,36 @@
 <?php
+    session_start();
+    
+    // initialize session variables if not set
+    if (!isset($_SESSION['score'])) {
+        $_SESSION['score'] = 0;
+        $_SESSION['questions_asked'] = 0;
+    }
+
+    $TOTAL_QUESTIONS = 10; // total number of questions in the quiz
+    $first_load = true;
+    $correct = false;
+
+    if (isset($_POST["guess"])) {
+        $first_load = false;
+        $_SESSION['questions_asked']++;
+
+        if($_POST["guess"] == $_POST["correct_guess"]) {
+            $correct = true;
+            $_SESSION['score']++;
+        }
+    }
+
+    // check if quiz is complete
+    $quiz_complete = $_SESSION['questions_asked'] >= $TOTAL_QUESTIONS;
+
+    // reset quiz if it's complete and user wants to start over
+    if (isset($_POST['restart'])) {
+        $_SESSION['score'] = 0;
+        $_SESSION['questions_asked'] = 0;
+        $quiz_complete = false;
+    }
+
     function read_file($filename)
     {
         $champion_type = array("MAGE", "ASSASSIN", "FIGHTER", "TANK", "SUPPORT", "MARKSMAN");
@@ -31,21 +63,6 @@
         return $choices;
     }
 
-    $first_load = true;
-
-    $correct = false;
-
-    if (isset($_POST["guess"]))
-    {
-
-        $first_load = false;
-
-        if($_POST["guess"] == $_POST["correct_guess"])
-        {
-            $correct = true;
-        }
-    }
-
 ?>
 <!doctype html>
 <html lang="en">
@@ -59,62 +76,70 @@
 </head>
 <body>
 <?php
-    $choices = read_file("champion_info_modified.txt");
-    $answer_index = rand(0, count($choices) - 1);
-    $ability = rand(2, 6);
-    $ability_choice = array("Champion", "Type", "Passive", "Q", "W", "E", "R");
-    list($correct_champion, $correct_type, $correct_passive, $correct_q, $correct_w, $correct_e, $correct_r) = explode(";", $choices[$answer_index]);
-    $correct_list = array($correct_champion, $correct_type, $correct_passive, $correct_q, $correct_w, $correct_e, $correct_r);
+    if (!$quiz_complete) {
+        $choices = read_file("champion_info_modified.txt");
+        $answer_index = rand(0, count($choices) - 1);
+        $ability = rand(2, 6);
+        $ability_choice = array("Champion", "Type", "Passive", "Q", "W", "E", "R");
+
+        list($correct_champion, $correct_type, $correct_passive, $correct_q, $correct_w, $correct_e, $correct_r) = explode(";", $choices[$answer_index]);
+        $correct_list = array($correct_champion, $correct_type, $correct_passive, $correct_q, $correct_w, $correct_e, $correct_r);
 ?>
-    <h1>AP League of Legends</h1>
+        <h1>AP League of Legends</h1>
+        <div class="score-info">
+            <p>Question <?= $_SESSION['questions_asked'] + 1 ?> of <?= $TOTAL_QUESTIONS ?></p>
+            <p>Current Score: <?= $_SESSION['score'] ?></p>
+        </div>
 
-    <h2>What is <?= $correct_champion ?>'s <?= $ability_choice[$ability] ?></h2>
+        <h2>What is <?= $correct_champion ?>'s <?= $ability_choice[$ability] ?></h2>
 
-
-    <form action="index.php" method="post">
-        <ol id="choices">
-            <?php
-            foreach($choices as $choice)
-            {
-                list($champion, $type, $passive, $q, $w, $e, $r) = explode(";", $choice);
-                $choice_list = array($champion, $type, $passive, $q, $w, $e, $r);
+        <form action="index.php" method="post">
+            <ol id="choices">
+                <?php
+                foreach($choices as $choice) {
+                    list($champion, $type, $passive, $q, $w, $e, $r) = explode(";", $choice);
+                    $choice_list = array($champion, $type, $passive, $q, $w, $e, $r);
+                    ?>
+                <li>
+                    <label>
+                        <input type="radio" name="guess" value="<?= $choice_list[$ability] ?>"/> <?= $choice_list[$ability] ?>
+                    </label>
+                </li>
+                <?php
+                }
                 ?>
-            <li>
-                <label>
-                    <input type="radio" name="guess" value="<?= $choice_list[$ability] ?>"/> <?= $choice_list[$ability] ?>
-                </label>
-            </li>
-            <?php
+            </ol>
+            <input type="hidden" name="correct_guess" value="<?= $correct_list[$ability] ?>">
+            <input type="hidden" name="correct_name" value="<?= $correct_list[0] ?>">
+            <input type="hidden" name="ability_type" value="<?= $ability_choice[$ability] ?>">
+            <input type="submit" value="Submit Answer">
+        </form>
+
+        <?php
+        if (!$first_load) {
+            if ($correct) {
+                ?><h1 class="feedback correct">Correct!</h1><?php
+            } else {
+                ?><h1 class="feedback incorrect">Sorry, try again.</h1><?php
             }
             ?>
-        </ol>
-        <input type="hidden" name="correct_guess" value="<?= $correct_list[$ability] ?>">
-        <input type="hidden" name="correct_name" value="<?= $correct_list[0] ?>">
-        <input type="hidden" name="ability_type" value="<?= $ability_choice[$ability] ?>">
-        <input type="submit" value="submit">
-    </form>
-
-
-    <?php
-    if (!$first_load)
-    {
-        if ($correct)
-        {
-            ?><h1>Correct!</h1> <?php
+            <h2><?= $_POST["correct_name"] ?>'s <?= $_POST["ability_type"] ?> is</h2>
+            <h2><?= $_POST["correct_guess"] ?></h2>
+            <?php
         }
-        else
-        {
-            ?><h1>Sorry, try again.</h1> <?php
-        }
-
+    } else {
+        // show final score and restart button
         ?>
-        <h2><?= $_POST["correct_name"] ?>'s <?= $_POST["ability_type"] ?> is</h2>
-        <h2><?= $_POST["correct_guess"] ?></h2>
+        <h1>Test Complete</h1>
+        <div class="final-score">
+            <h2>Your Final Score: <?= $_SESSION['score'] ?> out of <?= $TOTAL_QUESTIONS ?></h2>
+            <p>Percentage: <?= round(($_SESSION['score'] / $TOTAL_QUESTIONS) * 100) ?>%</p>
+            <form action="index.php" method="post">
+                <input type="submit" name="restart" value="Start New Quiz">
+            </form>
+        </div>
         <?php
     }
-
     ?>
-
-
 </body>
 </html>
